004507e5c8f31b8960710f1a6f163a69ed7bf7cf
charliemikels <charliemikels@gmail.com>
2021-10-17 21:34:29 -0400

    Converted Api.v to Json2. This also removes the need for the ApiJson struct.

--- api.v
+++ api.v
@@ -1,24 +1,20 @@
 module mcpkg
 
 import os
-import json
+import x.json2
 
-struct ApiJson {
+struct Api {
+mut:
 	mc_root_dir       string
 	mc_mods_dir       string
 	mcpkg_storage_dir string
 	auth_keys_path    string
-}
-
-pub struct Api {
-	ApiJson
-mut:
-	config_path   string
-	mod_platforms map[string]ModPlatform
-	auth_keys     map[string]string
+	config_path   		string
+	mod_platforms 		map[string]ModPlatform
+	auth_keys     		map[string]string
 	// current_branch Branch
 	// branches []Branch
-	notifications []Notification
+	notifications			[]Notification
 }
 
 struct Notification {
@@ -27,27 +23,43 @@ struct Notification {
 	urgency string = 'low' // "low", "med", "high"
 }
 
+pub fn (a Api) to_json() string {
+	mut obj := map[string]json2.Any
+	obj['mc_root_dir'] = a.mc_root_dir
+	obj['mc_mods_dir'] = a.mc_mods_dir
+	obj['mcpkg_storage_dir'] = a.mcpkg_storage_dir
+	obj['auth_keys_path'] = a.auth_keys_path
+	return obj.str()
+}
+
+pub fn (mut a Api) from_json(f json2.Any) {
+	obj := f.as_map()
+	for k, v in obj {
+		match k {
+			'mc_root_dir' {a.mc_root_dir = v.str()}
+			'mc_mods_dir' {a.mc_mods_dir = v.str()}
+			'mcpkg_storage_dir' {a.mcpkg_storage_dir = v.str()}
+			'auth_keys_path' {a.auth_keys_path = v.str()}
+			else {}
+		}
+	}
+}
+
 // load_api loads a configfile into an Api, or returns a default.
 pub fn load_api(path string) Api {
-	// Load main Api file
-	// quiet
-
-	mut api_json := ApiJson{}
+	mut api := Api{}
 	if path == 'tmp' {
-		// special case
 		fake_mc_root := os.join_path(os.temp_dir(), 'mcpkg_fake_mc_root')
-		api_json = ApiJson{
-			mc_root_dir: fake_mc_root
-			mc_mods_dir: os.join_path(fake_mc_root, 'mods')
-			mcpkg_storage_dir: os.join_path(fake_mc_root, 'mcpkg')
-			auth_keys_path: os.join_path(fake_mc_root, 'mcpkg', 'auth_keys.json')
-		}
+		api.mc_root_dir = fake_mc_root
+		api.mc_mods_dir = os.join_path(fake_mc_root, 'mods')
+		api.mcpkg_storage_dir = os.join_path(fake_mc_root, 'mcpkg')
+		api.auth_keys_path = os.join_path(fake_mc_root, 'mcpkg', 'auth_keys.json')
 	} else if path != '' {
 		println('Loading config file at `${os.real_path(path)}`...')
 		api_str := os.read_file(os.real_path(path)) or {
 			panic('Failed to read a file at `${os.real_path(path)}`.\n$err')
 		}
-		api_json = json.decode(ApiJson, api_str) or {
+		api = json2.decode<Api>(api_str) or {
 			panic('Failed to decode json at `${os.real_path(path)}`.\n$err')
 		}
 	} else {
@@ -59,25 +71,23 @@ pub fn load_api(path string) Api {
 			else { os.join_path(os.home_dir(), '.minecraft') }
 		}
 
-		api_json = ApiJson{
-			mc_root_dir: os_default_mc_dir
-			mc_mods_dir: os.join_path(os_default_mc_dir, 'mods')
-			mcpkg_storage_dir: os.join_path(os_default_mc_dir, 'mcpkg')
-			auth_keys_path: os.join_path(os_default_mc_dir, 'mcpkg', 'auth_keys.json')
-		}
-	}
-
-	mut api := Api{
-		ApiJson: api_json
-		config_path: path
+		api.mc_root_dir = os_default_mc_dir
+		api.mc_mods_dir = os.join_path(os_default_mc_dir, 'mods')
+		api.mcpkg_storage_dir = os.join_path(os_default_mc_dir, 'mcpkg')
+		api.auth_keys_path = os.join_path(os_default_mc_dir, 'mcpkg', 'auth_keys.json')
 	}
 
 	// Generated data:
 
 	// auth_keys
-	if os.exists(os.real_path(api_json.auth_keys_path)) {
-		auth_keys_file := os.read_file(os.real_path(api_json.auth_keys_path)) or { panic(err) }
-		api.auth_keys = json.decode(map[string]string, auth_keys_file) or { panic(err) }
+	if os.exists(os.real_path(api.auth_keys_path)) {
+		auth_keys_json := os.read_file(os.real_path(api.auth_keys_path)) or { panic(err) }
+		auth_keys_decoded := json2.raw_decode(auth_keys_json) or { panic(err) }
+		for k, v in auth_keys_decoded.as_map() {
+			api.auth_keys[k] = v.str()
+		}
+	} else if api.auth_keys_path != '' {
+		panic( 'Path to auth file was given, but no file exists at $api.auth_keys_path' )
 	}
 
 	// branches
